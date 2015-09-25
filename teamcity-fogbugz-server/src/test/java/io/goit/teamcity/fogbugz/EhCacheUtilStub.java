d19f1f90d894416bbbfda1ffbc87f2a7b6f4d498
Jozef Izso <jozef.izso@gmail.com>
2015-09-25 19:20:10 +0200

    Use in-memory cache store.

--- teamcity-fogbugz-server/src/test/java/io/goit/teamcity/fogbugz/EhCacheUtilStub.java
+++ teamcity-fogbugz-server/src/test/java/io/goit/teamcity/fogbugz/EhCacheUtilStub.java
@@ -5,10 +5,18 @@ import jetbrains.buildServer.serverSide.ServerPaths;
 import jetbrains.buildServer.util.EventDispatcher;
 import jetbrains.buildServer.util.cache.EhCacheUtil;
 import jetbrains.buildServer.util.cache.ResetCacheRegisterImpl;
+import net.sf.ehcache.Cache;
+import org.jetbrains.annotations.NotNull;
 
 public class EhCacheUtilStub extends EhCacheUtil {
 
     public EhCacheUtilStub() {
         super(new ServerPaths("", "", "", ""), EventDispatcher.create(BuildServerListener.class), new ResetCacheRegisterImpl());
+    }
+
+    @NotNull
+    @Override
+    public Cache createCache(@NotNull String name) {
+        return createInMemoryCache(name);
     }
 }
