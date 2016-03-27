354220d94b91f843d83645a71c64b621456cb926
Jozef Izso <jozef.izso@gmail.com>
2016-03-27 13:12:53 +0200

    Fixed usage of EhCacheUtil in tests.

--- teamcity-fogbugz-server/src/test/java/io/goit/teamcity/fogbugz/EhCacheUtilStub.java
+++ teamcity-fogbugz-server/src/test/java/io/goit/teamcity/fogbugz/EhCacheUtilStub.java
@@ -8,6 +8,8 @@ import jetbrains.buildServer.util.cache.ResetCacheRegisterImpl;
 import net.sf.ehcache.Cache;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
+
 public class EhCacheUtilStub extends EhCacheUtil {
 
     public EhCacheUtilStub() {
@@ -17,6 +19,18 @@ public class EhCacheUtilStub extends EhCacheUtil {
     @NotNull
     @Override
     public Cache createCache(@NotNull String name) {
-        return createInMemoryCache(name);
+        HashMap<String, String> params = new HashMap<String, String>();
+        params.put("%MAX_ELEMS_IN_MEMORY%", "1000");
+        params.put("%TTL_SECONDS%", "3600");
+        params.put("%TTL_IDLE_SECONDS%", "3600");
+        return this.createCache("<ehcache>" +
+            "  <defaultCache" +
+            "    maxElementsInMemory=\"%MAX_ELEMS_IN_MEMORY%\"" +
+            "    eternal=\"false\"" +
+            "    timeToIdleSeconds=\"%TTL_IDLE_SECONDS%\"" +
+            "    timeToLiveSeconds=\"%TTL_SECONDS%\"" +
+            "    overflowToDisk=\"false\"" +
+            "    diskPersistent=\"false\"/>" +
+            "</ehcache>", name, params);
     }
 }
